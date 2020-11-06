73b0958998d2a98d76b3313d635db105a1980edb
JJangl <71086028+JJangl@users.noreply.github.com>
2020-11-06 15:55:46 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,11 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+       int a=2;
+       int b=3;
+       String s="=Сумме целых чисел a и b";//напишите тут ваш код
+        System.out.println(a+b+" " +s);
+    }
+}
